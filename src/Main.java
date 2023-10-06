b771b284f0112f27c928e4eba22dd374ef709c4e
wenke.li <845990854@qq.com>
2023-10-06 10:29:36 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author: wenke.li
+ * @Description:
+ * @data: 2023/10/6 10:25
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
